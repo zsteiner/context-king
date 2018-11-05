8a2b98bd453870f85459492c3f5a4bc603690644
Zach Steiner <zsteiner@gmail.com>
2018-11-05 14:38:40 -0700

    Current temperature meter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import jsonp from 'jsonp';
+// import jsonp from 'jsonp';
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
 
 import Current from './components/Current/Current';
 
@@ -8,44 +9,86 @@ import styles from './styles/App.module.scss';
 
 const mockForecast = require('./mockData/mockForecast.json');
 
+const apiKeyMapbox =
+  'pk.eyJ1IjoienN0ZWluZXIiLCJhIjoiTXR4U0tyayJ9.6BxBAjPyMHbt1YfD5HWGXA';
+// const apiToken = '16eb53a912c674ef3028c1c421473d5e';
+
+const geocodingClient = mbxGeocoding({ accessToken: apiKeyMapbox });
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      forecast: {},
       currently: {},
       daily: {},
+      fetchingForecast: true,
+      forecast: {},
+      location: {},
       today: {}
     };
   }
 
   componentDidMount() {
     // this.getForecast();
+    this.getLocation();
+  }
+
+  getForecast = (lat, lon) => {
+    // const api = `https://api.darksky.net/forecast/${apiToken}/${lat},${lon}`;
+
+    // jsonp(api, null, (error, response) => {
+    //   if (error) {
+    //     console.error(error.message);
+    //   } else {
+    //     this.setState({
+    //       forecast: response,
+    //       fetchingForecast: false,
+    //       currently: response.currently,
+    //       daily: response.daily,
+    //       today: response.daily.data[0]
+    //     });
+    //   }
+    // });
 
     this.setState({
       forecast: mockForecast,
+      fetchingForecast: false,
       currently: mockForecast.currently,
       daily: mockForecast.daily,
       today: mockForecast.daily.data[0]
     });
-  }
+  };
 
-  getForecast = () => {
-    const apiToken = '16eb53a912c674ef3028c1c421473d5e';
-    const lat = 39.752394749115744;
-    const long = -105.00245891387033;
-    const api = `https://api.darksky.net/forecast/${apiToken}/${lat},${long}`;
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
 
-    jsonp(api, null, (error, response) => {
-      if (error) {
-        console.error(error.message);
-      } else {
+        this.lookUpLocation(lat, lon);
+        this.getForecast(lat, lon);
+      },
+      error => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+
+  lookUpLocation = (lat, lon) => {
+    geocodingClient
+      .reverseGeocode({
+        query: [lat, lon],
+        limit: 5
+      })
+      .send()
+      .then(response => {
+        const match = response.body;
+        console.log('lookup', match);
         this.setState({
-          forecast: response
+          location: match,
+          fetchingLocation: false
         });
-      }
-    });
+      });
   };
 
   render() {
@@ -53,11 +96,16 @@ class App extends Component {
 
     return (
       <article className={styles.app}>
-        <Current
-          temperature={state.currently.temperature}
-          temperatureHigh={state.today.temperatureHigh}
-          temperatureLow={state.today.temperatureLow}
-        />
+        <h2>{state.address}</h2>
+        {state.fetchingForecast ? (
+          <p>Getting Forecast</p>
+        ) : (
+          <Current
+            temperature={state.currently.temperature}
+            temperatureHigh={state.today.temperatureHigh}
+            temperatureLow={state.today.temperatureLow}
+          />
+        )}
       </article>
     );
   }
--- src/components/Current/Current.js
+++ src/components/Current/Current.js
@@ -4,11 +4,30 @@ import PropTypes from 'prop-types';
 import styles from './Current.module.scss';
 
 const Current = ({ temperature, temperatureHigh, temperatureLow }) => {
+  const currentRange = temperatureHigh - temperatureLow;
+  const currentDifference = temperature - temperatureLow;
+  let currentPercentage = currentDifference / currentRange;
+  const currentAngle = currentPercentage * 180 - 180;
+
+  let root = document.documentElement;
+  root.style.setProperty('--current-percentage', `${currentPercentage * 100}%`);
+  root.style.setProperty('--current-position', `${currentAngle}deg`);
+
   return (
     <div className={styles.current}>
-      {temperatureLow}
-      {temperature}
-      {temperatureHigh}
+      <div className={styles.currentTrack} />
+      <label className={styles.currentLabel}>
+        {Math.round(temperatureLow)}
+        <sup>&deg;</sup>
+      </label>
+      <span className={styles.currentTemperature}>
+        {Math.round(temperature)}
+        <sup>&deg;</sup>
+      </span>
+      <label className={styles.currentLabel}>
+        {Math.round(temperatureHigh)}
+        <sup>&deg;</sup>
+      </label>
     </div>
   );
 };
