be6cfffbf6fb44d393bbbabe1752d298fa8783c6
Zach Steiner <zsteiner@gmail.com>
2018-11-08 11:34:45 -0700

    refactor color function

--- src/utils/colorValue.js
+++ src/utils/colorValue.js
@@ -1,16 +1,28 @@
 export default function colorValue(value) {
   let calcValue;
 
-  if (value >= 80) {
-    calcValue = 12; // temp-high
-  } else if (value < 80 && value >= 65) {
-    calcValue = 29; // temp-med-high
-  } else if (value < 65 && value >= 40) {
-    calcValue = 133; // temp-med-low
-  } else if (value < 40 && value >= 32) {
-    calcValue = 209; // temp-low
-  } else {
-    calcValue = 192; // temp-ex-low
+  const temps = {
+    high: 80,
+    medHigh: 60,
+    medLow: 40,
+    low: 30
+  };
+
+  switch (true) {
+    case value >= temps.high:
+      calcValue = 12; // temp-high
+      break;
+    case value < temps.high && value >= temps.medHigh:
+      calcValue = 29; // temp-med-high
+      break;
+    case value < temps.medHigh && value >= temps.medLow:
+      calcValue = 133; // temp-med-low
+      break;
+    case value < temps.medLow && value >= temps.low:
+      calcValue = 209; // temp-low
+      break;
+    default:
+      calcValue = 192; // temp-ex-low
   }
 
   return calcValue;
