cc9f0bbb1acceca1a28088d3c75887d8d1202eb7
Zach Steiner <zsteiner@gmail.com>
2018-11-08 10:03:54 -0700

    Updates styles and layout

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,18 +3,19 @@ import React from 'react';
 import { LocationContext } from '../../contexts/LocationContext';
 
 import CurrentMeter from '../CurrentMeter/CurrentMeter';
+import styles from './Forecast.module.scss';
 
 const Forecast = () => {
   return (
     <LocationContext.Consumer>
-      {context => [
-        <p>{context.forecast.daily.summary}</p>,
-        <CurrentMeter forecast={context.forecast} />
-      ]}
+      {context => (
+        <section className={styles.forecast}>
+          <p>{context.forecast.daily.summary}</p>
+          <CurrentMeter forecast={context.forecast} />
+        </section>
+      )}
     </LocationContext.Consumer>
   );
 };
-
-Forecast.contextType = LocationContext;
 
 export default Forecast;
--- src/utils/colorValue.js
+++ src/utils/colorValue.js
@@ -5,9 +5,9 @@ export default function colorValue(value) {
     calcValue = 12; // temp-high
   } else if (value < 80 && value >= 65) {
     calcValue = 29; // temp-med-high
-  } else if (value < 65 && value >= 45) {
+  } else if (value < 65 && value >= 40) {
     calcValue = 133; // temp-med-low
-  } else if (value < 45 && value >= 32) {
+  } else if (value < 40 && value >= 32) {
     calcValue = 209; // temp-low
   } else {
     calcValue = 192; // temp-ex-low
