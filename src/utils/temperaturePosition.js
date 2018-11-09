dcabad5fe418f1efc278d90de353616d4a681504
Zach Steiner <zsteiner@gmail.com>
2018-11-09 10:10:02 -0700

    Error handling for current meter

--- src/utils/temperaturePosition.js
+++ src/utils/temperaturePosition.js
@@ -3,10 +3,17 @@ export default function temperaturePosition(
   temperatureHigh,
   temperatureLow
 ) {
+  if (temperature < temperatureLow) {
+    temperature = temperatureLow;
+  } else if (temperature > temperatureHigh) {
+    temperature = temperatureHigh;
+  }
+
   const currentRange = temperatureHigh - temperatureLow;
   const currentDifference = temperature - temperatureLow;
+
   let currentPercentage = currentDifference / currentRange;
-  const currentAngle = currentPercentage * 180 - 180;
+  let currentAngle = currentPercentage * 180 - 180;
 
   let root = document.documentElement;
   root.style.setProperty('--current-position', `${currentAngle}deg`);
