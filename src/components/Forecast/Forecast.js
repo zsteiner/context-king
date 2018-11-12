c32d0935bae189fb55d9d93beb962a2af5fd1aec
Zach Steiner <zsteiner@gmail.com>
2018-11-12 08:00:52 -0700

    Add feels like

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -4,9 +4,10 @@ import { LocationContext } from '../../contexts/LocationContext';
 
 import CurrentMeter from '../CurrentMeter/CurrentMeter';
 import DailyForecast from '../DailyForecast/DailyForecast';
+import Temperature from '../Temperature/Temperature';
+import WeatherIcon from '../WeatherIcon/WeatherIcon';
 
 import styles from './Forecast.module.scss';
-import WeatherIcon from '../WeatherIcon/WeatherIcon';
 
 const Forecast = () => {
   return (
@@ -16,15 +17,23 @@ const Forecast = () => {
           <header className={styles.forecastCurrent}>
             <CurrentMeter forecast={context.forecast} />
             <div className={styles.forecastInfo}>
-              <h2 className={styles.forecastConditions}>
+              <div className={styles.forecastConditions}>
                 <WeatherIcon
                   conditions={context.forecast.currently.icon}
                   className={styles.icon}
                 />
-                {context.forecast.currently.summary}
-              </h2>
+                <div>
+                  <h2>{context.forecast.currently.summary}</h2>
+                  <p className={styles.forecastText}>
+                    Feels like{' '}
+                    <Temperature
+                      temp={context.forecast.currently.apparentTemperature}
+                    />
+                  </p>
+                </div>
+              </div>
 
-              <p>
+              <p className={styles.forecastText}>
                 <strong className={styles.forecastSummary}>Today: </strong>
                 {context.forecast.daily.data[0].summary}
               </p>
