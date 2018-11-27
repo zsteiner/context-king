08740d24fed027aaf7e6e5d599d169a59f478486
Zach Steiner <zsteiner@gmail.com>
2018-11-27 12:30:21 -0700

    Refactor map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,20 +5,67 @@ import styles from './Map.module.scss';
 
 class Map extends Component {
   static propTypes = {
-    coordinates: PropTypes.array
+    coordinates: PropTypes.array,
+    fieldControl: PropTypes.bool,
+    mapField: PropTypes.oneOf([
+      'temperature',
+      'precipitation_rate',
+      'radar',
+      'apparent_temperature',
+      'cloud_cover',
+      'wind_speed',
+      'wind_gust',
+      'dew_point',
+      'uv_index',
+      'sea_level_pressure',
+      'ozone'
+    ]),
+    timeControl: PropTypes.bool,
+    units: PropTypes.oneOf(['standard', 'metric']),
+    zoom: PropTypes.number
   };
 
+  static defaultProps = {
+    mapField: 'radar',
+    fieldControl: false,
+    timeControl: false,
+    units: 'standard',
+    zoom: 7
+  };
+
+  uom(units) {
+    switch (this.props.mapField) {
+      case 'temperature':
+      case 'apparent_temperature':
+      case 'dew_point':
+        return units === 'metric' ? '_c' : '_f';
+      case 'radar':
+        return units === 'metric' ? '_mmph' : '_inph';
+      case 'wind_gust':
+      case 'wind_speed':
+        return units === 'metric' ? '_kmph' : '_mph';
+      case 'sea_level_pressure':
+        return units === 'metric' ? '_hpa' : '_inhg';
+      case 'ozone':
+        return '_du';
+      default:
+        return '';
+    }
+  }
+
   render() {
-    const { coordinates } = this.props;
+    const {
+      coordinates,
+      fieldControl,
+      mapField,
+      timeControl,
+      units,
+      zoom
+    } = this.props;
 
-    const field = 'radar';
-    const zoom = 7;
-    const timeControl = false;
-    const fieldControl = false;
+    const uom = this.uom(units);
 
-    const url = `https://maps.darksky.net/@${field},${coordinates[1]},${
-      coordinates[0]
-    },${zoom}?embed=true&timeControl=${timeControl}&fieldControl=${fieldControl}&defaultField=${field}`;
+    const url = `https://maps.darksky.net/@${mapField},${coordinates[1].toString()},${coordinates[0].toString()},${zoom.toString()}?embed=true&timeControl=${timeControl.toString()}&fieldControl=${fieldControl.toString()}&defaultField=${mapField}&defaultUnits=${uom}`;
 
     return (
       <section className={`${styles.map} embed`} ref="map">
