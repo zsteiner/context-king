db97935dffa51eb53dd4211d96af8538b0b12425
Zach Steiner <zsteiner@gmail.com>
2018-11-29 11:46:12 -0700

    Update refresh label

--- src/components/LocationName/LocationName.js
+++ src/components/LocationName/LocationName.js
@@ -6,11 +6,24 @@ import styles from './LocationName.module.scss';
 
 const LocationName = ({
   coordinates,
+  fetchingForecast,
   forecastRefresh,
   locationName,
   updateDate
 }) => {
   const dateString = updateDateString(updateDate);
+  let refreshLabel;
+
+  switch (true) {
+    case forecastRefresh && !fetchingForecast:
+      refreshLabel = 'New Forecast';
+      break;
+    case fetchingForecast:
+      refreshLabel = 'Getting Location';
+      break;
+    default:
+      refreshLabel = 'Existing Forecast';
+  }
 
   return (
     <React.Fragment>
@@ -20,9 +33,7 @@ const LocationName = ({
           ? `${coordinates[0]}, ${coordinates[1]}`
           : '0.00, 0.00'}
       </small>
-      <small className={styles.locationRefresh}>
-        {forecastRefresh ? 'New Forecast' : 'Existing Forecast'}
-      </small>
+      <small className={styles.locationRefresh}>{refreshLabel}</small>
       <small className={styles.locationRefresh}>Updated {dateString}</small>
     </React.Fragment>
   );
