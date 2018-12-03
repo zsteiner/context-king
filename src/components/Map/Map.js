6b135300b9023d69b9a543494219a445c1c09522
Zach Steiner <zsteiner@gmail.com>
2018-12-03 07:40:07 -0700

    Refactor maps component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,40 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Map.module.scss';
 
-class Map extends Component {
-  static propTypes = {
-    coordinates: PropTypes.array,
-    fieldControl: PropTypes.bool,
-    mapField: PropTypes.oneOf([
-      'temperature',
-      'precipitation_rate',
-      'radar',
-      'apparent_temperature',
-      'cloud_cover',
-      'wind_speed',
-      'wind_gust',
-      'dew_point',
-      'uv_index',
-      'sea_level_pressure',
-      'ozone'
-    ]),
-    timeControl: PropTypes.bool,
-    units: PropTypes.oneOf(['standard', 'metric']),
-    zoom: PropTypes.number
-  };
-
-  static defaultProps = {
-    mapField: 'radar',
-    fieldControl: false,
-    timeControl: false,
-    units: 'standard',
-    zoom: 7
-  };
-
-  uom(units) {
-    switch (this.props.mapField) {
+const Map = ({
+  coordinates,
+  fieldControl,
+  mapField,
+  timeControl,
+  units,
+  zoom
+}) => {
+  const calcUnits = units => {
+    switch (mapField) {
       case 'temperature':
       case 'apparent_temperature':
       case 'dew_point':
@@ -51,28 +29,46 @@ class Map extends Component {
       default:
         return '';
     }
-  }
+  };
 
-  render() {
-    const {
-      coordinates,
-      fieldControl,
-      mapField,
-      timeControl,
-      units,
-      zoom
-    } = this.props;
+  const uom = calcUnits(units);
 
-    const uom = this.uom(units);
+  const url = `https://maps.darksky.net/@${mapField},${coordinates[1].toString()},${coordinates[0].toString()},${zoom.toString()}?embed=true&timeControl=${timeControl.toString()}&fieldControl=${fieldControl.toString()}&defaultField=${mapField}&defaultUnits=${uom}`;
 
-    const url = `https://maps.darksky.net/@${mapField},${coordinates[1].toString()},${coordinates[0].toString()},${zoom.toString()}?embed=true&timeControl=${timeControl.toString()}&fieldControl=${fieldControl.toString()}&defaultField=${mapField}&defaultUnits=${uom}`;
+  return (
+    <section className={`${styles.map} embed`}>
+      <iframe frameBorder="0" title="Map" src={url} />
+    </section>
+  );
+};
 
-    return (
-      <section className={`${styles.map} embed`} ref="map">
-        <iframe frameBorder="0" title="Map" src={url} />
-      </section>
-    );
-  }
-}
+Map.propTypes = {
+  coordinates: PropTypes.array,
+  fieldControl: PropTypes.bool,
+  mapField: PropTypes.oneOf([
+    'temperature',
+    'precipitation_rate',
+    'radar',
+    'apparent_temperature',
+    'cloud_cover',
+    'wind_speed',
+    'wind_gust',
+    'dew_point',
+    'uv_index',
+    'sea_level_pressure',
+    'ozone'
+  ]),
+  timeControl: PropTypes.bool,
+  units: PropTypes.oneOf(['standard', 'metric']),
+  zoom: PropTypes.number
+};
+
+Map.defaultProps = {
+  mapField: 'radar',
+  fieldControl: false,
+  timeControl: false,
+  units: 'standard',
+  zoom: 7
+};
 
 export default Map;
