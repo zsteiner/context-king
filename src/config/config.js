6a99dda8afd4ff3afacc2ece79fbec2def9a726e
Zach Steiner <zsteiner@gmail.com>
2019-08-01 10:54:49 -0600

    Remove API keys

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,6 @@
 const config = {
-  darkskyKey: '16eb53a912c674ef3028c1c421473d5e',
-  mapboxKey:
-    'pk.eyJ1IjoienN0ZWluZXIiLCJhIjoiTXR4U0tyayJ9.6BxBAjPyMHbt1YfD5HWGXA',
-  flickrKey: '74385ce67a6b86ae4d7423caf1f8088d',
-  giphyKey: 'yBMBCs3nUSiT9uEIygbSfchXGamjM9gv',
+  darkskyKey: process.env.REACT_APP_DARK_SKY,
+  mapboxKey: process.env.REACT_APP_MAPBOX,
   conditionList: [
     'apparentTemperature',
     'humidity',
